108a7ac01e842cc3ad01f242a308df757cc46029
waviq <waviq.subkhi@gmail.com>
2016-07-30 05:54:50 +0700

    update pengurangan

---
+++ method.py
@@ -0,0 +1,26 @@
+def penjumlahan(a, b):
+    print(a + b)
+
+
+penjumlahan(4, 6)
+
+
+def nama(nama):
+    if(nama == "waviq subhi"):
+        return print("waviq")
+    namaInput = input("masukan nama: ")
+    result = nama(namaInput)
+
+    print("nama is ", result)
+
+
+
+def pengurangan(c, d):
+    print(a - b)
+
+pengurangan
+
+# def total(harga=1):
+#     print(harga * 10, "$")
+#
+# total()
